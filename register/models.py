9669b5ce076235ba5aa2f10de596b2313ebce195
Kyaw Min Lwin <kyawminlwinprogrammer@gmail.com>
2023-12-19 19:43:52 +0630

    added task management

--- register/models.py
+++ register/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from datetime import datetime
+
 
 # Create your models here.
+class Tasks(models.Model):
+    name = models.CharField(max_length=10000)
+    time = models.DateTimeField(default=datetime.now)
--- register/views.py
+++ register/views.py
@@ -2,17 +2,29 @@ from django.shortcuts import render, HttpResponse, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from .forms import UserRegistrationForm
+from django.contrib.auth.decorators import login_required
+from .models import Tasks
+
 
 def register(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = UserRegistrationForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data['username']
-            messages.success(request, f'{username} has been registered')
-            return redirect('index')
+            username = form.cleaned_data["username"]
+            messages.success(request, f"{username} has been registered")
+            return redirect("index")
     form = UserRegistrationForm()
-    return render(request, 'register.html', {'form': form})
+    return render(request, "register.html", {"form": form})
 
+
+@login_required
 def index(request):
-    return render(request, 'index.html')
+    if request.method == "POST":
+        task = request.POST.get("task")
+        print(task)
+        new_task = Tasks.objects.create(name=task)
+        new_task.save()
+        tasks = Tasks.objects.all()
+        return render(request, "index.html", {"tasks": tasks})
+    return render(request, "index.html")
